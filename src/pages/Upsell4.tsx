e833ec371110708a024c8c02cf43ac4e366e3fd5
GREGALMEID <gregalmeid@gmail.com>
2025-05-23 20:17:40 -0300

    Deploy application to Netlify

--- src/pages/Upsell4.tsx
+++ src/pages/Upsell4.tsx
@@ -145,7 +145,7 @@ const Upsell4: React.FC = () => {
                 </button>
 
                 <p className="text-sm text-gray-500">
-                 O Valor pago da inscrição anterior <br />
+                 O Valor anterior pago <br />
                   será reembolsado.
                 </p>
               </div>
