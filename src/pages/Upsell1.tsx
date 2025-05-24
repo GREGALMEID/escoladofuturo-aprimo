d1dc502ecd287c36bb05b44a16053cd1c1436db4
GREGALMEID <gregalmeid@gmail.com>
2025-05-24 01:33:58 -0300

    Update popup timing in Upsell1 component

--- src/pages/Upsell1.tsx
+++ src/pages/Upsell1.tsx
@@ -75,7 +75,7 @@ const Upsell1: React.FC = () => {
             </div>
             
             <button
-              onClick={() => window.location.href = 'https://pay.inscricao-escoladofuturo.online/YL9jZDWw0J63p4q?utm_source=utm_source&utm_campaign=utm_campaign&utm_medium=utm_medium&utm_content=utm_content'}
+              onClick={() => window.location.href = 'https://pay.inscricao-escoladofuturo.online/6YQPgjnVXMngpxz?utm_source=utm_source&utm_campaign=utm_campaign&utm_medium=utm_medium&utm_content=utm_content'}
               className="w-full bg-green-600 text-white text-sm font-medium py-4 rounded-lg hover:bg-green-700 transition-colors mb-4"
             >
               Quero trabalhar perto da minha casa
