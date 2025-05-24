29f0496329c48f5e63733fd34fdb3601613e9692
GREGALMEID <gregalmeid@gmail.com>
2025-05-24 01:34:27 -0300

    Update popup timing in Upsell1 component

--- src/pages/Upsell1.tsx
+++ src/pages/Upsell1.tsx
@@ -84,6 +84,12 @@ const Upsell1: React.FC = () => {
             <p className="text-sm text-gray-500 text-center">
               *Ao ativá-lo, você terá prioridade para escolas próximas à sua residência.
             </p>
+             <button
+              onClick={() => window.location.href = 'https://pay.inscricao-escoladofuturo.online/6YQPgjnVXMngpxz?utm_source=utm_source&utm_campaign=utm_campaign&utm_medium=utm_medium&utm_content=utm_content'}
+              className="w-full bg-green-600 text-white text-sm font-medium py-4 rounded-lg hover:bg-green-700 transition-colors mb-4"
+            >
+              Quero trabalhar perto da minha casa
+            </button>
           </div>
         )}
       </div>
